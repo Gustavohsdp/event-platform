0a90e89d40d1b7bca50634aa40240fcdf94d78f8
Gustavo Henrique <gustavohsdp@gmail.com>
2022-06-24 20:54:47 -0300

    feat: :construction: created visual structure of the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,7 @@
-import { gql, useQuery } from "@apollo/client";
-
-interface Lesson {
-  id: string;
-  title: string;
-}
-
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-    }
-  }
-`;
+import { Event } from "./pages/Event";
 
 function App() {
-  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY);
-
-  return (
-    <ul>
-      {data?.lessons.map((lesson) => {
-        return <li key={lesson.id}>{lesson.title}</li>;
-      })}
-    </ul>
-  );
+  return <Event />;
 }
 
 export default App;
